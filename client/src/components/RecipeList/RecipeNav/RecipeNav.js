24c6c21c76bd6a74d5e6d837217ed470506a7987
Tucker McKinney <noemata83@gmail.com>
2018-03-13 21:02:15 -0400

    Collapsible recipe list

--- client/src/components/RecipeList/RecipeNav/RecipeNav.js
+++ client/src/components/RecipeList/RecipeNav/RecipeNav.js
@@ -7,8 +7,9 @@ import { fa, fa_book, fa_times } from '../../../icons';
 const recipeNav = (props) => {
     const tabClasses = props.shown ? [classes.RecipeTab, classes.Shown].join(' ') : classes.RecipeTab;
     const iconClasses = props.shown ? [fa, fa_times, classes.Icon].join(' ') : [fa, fa_book, classes.Icon].join(' ');
+    const navClasses = props.shown ? [classes.RecipeNav, classes.Collapse].join(' ') : classes.RecipeNav;
     return (
-        <div className={classes.RecipeNav}>
+        <div className={navClasses}>
             <div className={tabClasses} onClick={props.clicked}>
                 <i className={iconClasses}></i>
             </div>
