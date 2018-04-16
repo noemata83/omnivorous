05676aaaa15d0e5aab76efcdc6e713ce4bba2cf0
Tucker McKinney <noemata83@gmail.com>
2018-04-15 23:14:27 -0400

    Noun Project Icons for bottom nav on mobile - need an about page with attributions

--- client/src/containers/Dashboard/Dashboard.js
+++ client/src/containers/Dashboard/Dashboard.js
@@ -12,6 +12,10 @@ import * as actions from '../../store/actions';
 import {Tab, Tabs} from 'material-ui/Tabs';
 import { cyan400 } from 'material-ui/styles/colors';
 import withSizes from 'react-sizes';
+import CookBook from '../../assets/img/noun_51422_cc.svg'; // Cookbook Icon by Marcella Cigarini for the Noun Project
+import RecipeIcon from '../../assets/img/noun_1142064_cc.svg'; // Cooking Icon by Dinosoft Labs for the Noun Project
+import ShoppingListIcon from '../../assets/img/noun_1351304_cc.svg'; // Shopping List Icon by Gregor Cresnar for the Noun Project
+import MealPlanIcon from '../../assets/img/noun_689869_cc.svg'; // Calendar Icon by Smidt Sergey, US for the Noun Project
 
 // import Wrapper from '../../hoc/Wrapper/Wrapper';
 
@@ -77,17 +81,17 @@ class Dashboard extends Component {
                             <Header />
                         </div>
                         <div className={classes.MainWindow}>
-                            <Tabs value={this.state.tab} onChange={this.handleTabChange} inkBarStyle={this.props.isMobile ? {} : {display: 'none'}} tabItemContainerStyle={this.props.isMobile ? {position: 'absolute', bottom: 0, display: 'block', width: '100%'} : {display: 'none'}}>
-                                <Tab value='recipe' style={{backgroundColor:cyan400}}  label="Recipes">
+                            <Tabs value={this.state.tab} onChange={this.handleTabChange} inkBarStyle={this.props.isMobile ? {} : {display: 'none'}} tabItemContainerStyle={this.props.isMobile ? {position: 'fixed', bottom: 0, display: 'block', width: '100%', zIndex:100} : {display: 'none'}}>
+                                <Tab value='recipe' style={{backgroundColor:cyan400}}  label={<img src={RecipeIcon} alt="Recipe Icon" style={{height: '36px', width: '36px'}} />}>
                                     {mainWindow}
                                 </Tab>
-                                <Tab value='list' style={{backgroundColor:cyan400}}  label="Cookbook">
+                                <Tab value='list' style={{backgroundColor:cyan400}}  label={<img src={CookBook} alt="CookBook icon" style={{height: '36px', width: '36px'}}/>}>
                                     <RecipeList setEditMode={this.setEditModeHandler} displayRecipe={this.displayRecipe}/>
                                 </Tab>
-                                <Tab value='shopping'  style={{backgroundColor:cyan400}} label="Shopping List">
+                                <Tab value='shopping'  style={{backgroundColor:cyan400}} label={<img src={ShoppingListIcon} alt="Shopping List Icon" style={{height: '36px', width: '36px'}}/>}>
                                     <ShoppingListControl />
                                 </Tab>
-                                <Tab value='plan'  style={{backgroundColor:cyan400}} label="Meal Plan">
+                                <Tab value='plan'  style={{backgroundColor:cyan400}} label={<img src={MealPlanIcon} alt="Meal Plan Icon" style={{height: '36px', width: '36px'}}/>}>
                                     <div>Hi, I don't exist yet</div>
                                 </Tab>
                             </Tabs>
