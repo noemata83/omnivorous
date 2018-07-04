a247d31b8a2d157dd731811854e95ff9e5a6dbb3
Tucker McKinney <noemata83@gmail.com>
2018-07-03 21:06:05 -0400

    Add boilerplate delete route to meal controller

--- api/controllers/mealController.js
+++ api/controllers/mealController.js
@@ -1,36 +1,53 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
-const User = mongoose.model('users');
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
 // const Recipe = mongoose.model('Recipes');
-const Meal = mongoose.model('Meals');
+const Meal = mongoose.model("Meals");
 
 const createMeal = (req, res) => {
-    User.findById(req.user._id, (err, foundUser) => {
+  User.findById(req.user._id, (err, foundUser) => {
+    if (err) {
+      res.send(err);
+    } else {
+      const newMeal = req.body;
+      Meal.create(newMeal, (err, meal) => {
         if (err) {
-            res.send(err);
+          res.send(err);
         } else {
-            const newMeal = req.body;
-            Meal.create(newMeal, (err, meal) => {
-                if (err) {
-                    res.send(err);
-                } else {
-                    meal.save();
-                    foundUser.meals.push(recipe);
-                    foundUser.save();
-                    res.json(meal);
-                    }
-                });
-            }
-        });
+          meal.save();
+          foundUser.meals.push(recipe);
+          foundUser.save();
+          res.json(meal);
+        }
+      });
     }
+  });
+};
 
-    const getMeals = (req, res) => {
-        User.findById(req.user._id).then( user => {
-            User.populate(user, { path: 'meals', populate: 'meal'}, (err, user) => {
-                res.json(user.meals);        
-            });
-        });
+const getMeals = (req, res) => {
+  User.findById(req.user._id).then(user => {
+    User.populate(user, { path: "meals", populate: "meal" }, (err, user) => {
+      res.json(user.meals);
+    });
+  });
+};
+
+const deleteMeal = (req, res) => {
+  User.findById(req.user._id).then(user => {
+    if (!user) {
+      res.send("Something went wrong.");
+    } else {
+      user.meals = user.meals.filter(
+        meal => meal.toHexString() !== req.params.id
+      );
+      user.save();
     }
-
-    
+    Meal.findByIdAndRemove(req.params.id, err => {
+      if (err) {
+        return res.send(err);
+      }
+      res.json({ message: "Meal Deleted." });
+    });
+  });
+}
