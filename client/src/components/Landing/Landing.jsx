dd9279323efbee4a8fde4bfea5e1bac26ec34757
Tucker McKinney <noemata83@gmail.com>
2019-09-03 21:38:58 -0400

    Status update

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -9,9 +9,8 @@ import addToShoppingList from '../../assets/img/add-to-shopping-list.gif';
 
 import Background from '../UI/Background/Background';
 
-const landing = props =>
-  (
-    <div className={classes.LandingPage}>
+const landing = props => (
+  <div className={classes.LandingPage}>
     <div className={classes.Header}>
       <Background />
       <div className={['card', classes.TextBox].join(' ')}>
@@ -28,65 +27,82 @@ const landing = props =>
         <br />
         <p style={{ fontSize: '1.4rem', textAlign: 'left' }}>
           <strong>Current Status:</strong>
+          <br />
           <em>
-            Migrated Presentational Components to Material UI v1.0; shopping list and (mobile) sidebar
-            need fixes. Recipe Import now features CRF microservice-powered intelligent ingredient parsing.
-            <br />Check back often for updates!
-          </em><br />
+            Ingredient parser service is temporarily offline. Recipe creation
+            and import will not work. I hope to restore this functionality as
+            soon as time permits!
+            <br />
+            Check back often for updates!
+          </em>
+          <br />
         </p>
       </div>
     </div>
     <div>
-    <div style={{background:"-webkit-linear-gradient(top, #80DEEA, #FFF)"}}>
-      <section className={classes.Description} style={{paddingTop: '20rem'}}>
+      <div
+        style={{ background: '-webkit-linear-gradient(top, #80DEEA, #FFF)' }}
+      >
+        <section
+          className={classes.Description}
+          style={{ paddingTop: '20rem' }}
+        >
           <Card className={classes.MarketingCard}>
             <div className={classes.Graphic}>
               <img
                 src={addRecipe}
                 alt="Importing Recipes is Easy!"
-                style={{width: '100%'}}
-                />
+                style={{ width: '100%' }}
+              />
             </div>
             <div className={classes.Text}>
               <h2 className={classes.TextHeader}>Manage Your Recipes</h2>
-              <p style={{fontSize:'2.4rem', fontWeight: 'bold'}}>Omnivorous helps you gather recipes and organize them, so your next meal is only a few clicks away.</p>
+              <p style={{ fontSize: '2.4rem', fontWeight: 'bold' }}>
+                Omnivorous helps you gather recipes and organize them, so your
+                next meal is only a few clicks away.
+              </p>
             </div>
           </Card>
-      </section>
-      <section className={classes.Description}>
+        </section>
+        <section className={classes.Description}>
           <Card className={classes.MarketingCard}>
             <div className={classes.Text}>
-                <h2 className={classes.TextHeader}>Streamline Your Shopping</h2>
-                <p style={{fontSize:'1.8rem'}}>Omnivorous lets you easily build and organize shopping lists. Add recipe ingredients to your shopping list with one click, and drag and drop
-                  to organize them.</p>
+              <h2 className={classes.TextHeader}>Streamline Your Shopping</h2>
+              <p style={{ fontSize: '1.8rem' }}>
+                Omnivorous lets you easily build and organize shopping lists.
+                Add recipe ingredients to your shopping list with one click, and
+                drag and drop to organize them.
+              </p>
             </div>
-              <div className={classes.Graphic}>
-                <img 
-                  src={addToShoppingList}
-                  alt="Add ingredients to the shopping list with a single click!"
-                  style={{width: '100%'}}
-                />
-              </div>
+            <div className={classes.Graphic}>
+              <img
+                src={addToShoppingList}
+                alt="Add ingredients to the shopping list with a single click!"
+                style={{ width: '100%' }}
+              />
+            </div>
           </Card>
-      </section>
-      <section className={classes.Description}>
-        <Card className={classes.MarketingCard}>
-          <div className={classes.Graphic}>
-            Coming Soon!
-          </div>
-          <div className={classes.Text}>
-            <h2 className={classes.TextHeader}>Plan Your Week</h2>
-            <p style={{fontSize:'1.8rem'}}>Drag and drop recipes onto your weekly meal plan. Build a shopping list from your meal plan in seconds!</p>
-          </div>
-        </Card>
-      </section>
-    </div>
+        </section>
+        <section className={classes.Description}>
+          <Card className={classes.MarketingCard}>
+            <div className={classes.Graphic}>Coming Soon!</div>
+            <div className={classes.Text}>
+              <h2 className={classes.TextHeader}>Plan Your Week</h2>
+              <p style={{ fontSize: '1.8rem' }}>
+                Drag and drop recipes onto your weekly meal plan. Build a
+                shopping list from your meal plan in seconds!
+              </p>
+            </div>
+          </Card>
+        </section>
+      </div>
       <footer>
-        An app by Tucker McKinney. Last updated: {new Date(Date.now()).toLocaleDateString()}
+        An app by Tucker McKinney. Last updated:{' '}
+        {new Date(Date.now()).toLocaleDateString()}
       </footer>
     </div>
   </div>
-  );
+);
 
 const mapStateToProps = state => ({
   currentUser: state.auth.userId,
